70670c7f6c9aea3ff609891e73b7a3e9f23c089f
Simonbunse <Simon.hastrup.jensen@gmail.com>
2024-12-09 11:54:10 +0100

    blablabla

--- nextjs-app/next.config.mjs
+++ nextjs-app/next.config.mjs
@@ -1,9 +1,5 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  server: {
-    host: '0.0.0.0', // Ensure Next.js binds to all interfaces
-  },
 };
 
 export default nextConfig;
